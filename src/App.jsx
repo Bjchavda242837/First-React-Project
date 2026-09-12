7f375619e1a68d010121d79940bf0b055c01a775
Bhavesh Chavda <bjchavda746525@gmail.com>
2026-09-12 16:25:23 +0530

    Build React counter with useState and useEffect

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <CountIncreaseComp /> // manual Counter Called.
+      <CountTimerComp />    // timer Count component called.
+    </div>
+  );
+}
+
+// timer component
+function CountTimerComp() {
+
+  // State variable = useState hook 
+  const [count, setCount] = useState(0);
+
+  // State varible update function
+  function increaseCount() {
+    console.log("inside of setInterval function");
+    setCount((prev) => prev + 1);
+  }
+
+  // useEffect Hook used for setInterval because i have to run only once when app mount for the first time.
+  useEffect(() => {
+
+    console.log("inside of useEffects");
+    // setInterval defined and create a variable for clearInterval.
+    const timer = setInterval(increaseCount, 1000);
+
+    // timer cleanup in useEffect hook.
+    return ()=> {
+      clearInterval(timer);
+    }
+
+  }, []);
+
+  // html Div with p tag and count variable in return for rendering
+  return (
+    <div>
+      <p> {count} timer</p>
+    </div>
+  );
+}
+
+// button click increase counter component.
+function CountIncreaseComp() {
+
+  // State variable = useState hook.
+  const [count, setCount] = useState(0);
+
+  // function for increase count on onclick.
+  function IncreaseCountClick() {
+
+    // state variable is update here via setCount function.
+    setCount(count + 1);
+  }
+
+  // html Div with p tag and count variable in return for rendering
+  return (
+    <div>
+      <p> {count} </p>
+      <button onClick={IncreaseCountClick}>Increase</button>
+    </div>
+  );
+}
+
+export default App;
